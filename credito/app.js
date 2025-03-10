b677d69ca18e2709717f8c70a426c2f6e2af3d33
profecazarez <manuel.cazarez@info.uas.edu.mx>
2025-03-10 08:38:54 -0700

    Ejemplo de API credito

---
+++ credito/app.js
@@ -0,0 +1,27 @@
+const express = require ('express')
+const fs = require('fs') // file system (sistema de archivos)
+const app = express()
+const puerto = 3000
+
+// configurar body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// crear servidor web
+app.listen(puerto, () => {
+    console.log('Servidor web iniciado')
+})
+
+// crear ruta
+app.post('/credito', (req, res) => {
+    const credito = req.body;
+    const { numeroCredito } = credito;
+    const path = `./credito-${numeroCredito}.json`;
+
+    // crear el archivo JSON
+    fs.writeFile(path, JSON.stringify(credito), (err) => {
+        res.send('OK');
+    })
+
+})
